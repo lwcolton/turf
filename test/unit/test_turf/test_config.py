39d38ac45f906265a8a759b976a0034edc278fb6
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-02-06 11:40:55 -0500

    add some tests

--- test/unit/test_turf/test_config.py
+++ test/unit/test_turf/test_config.py
@@ -1,6 +1,16 @@
+import random
 from unittest import mock
+import uuid
+
+from nose2.tools import params
 
 from turf.config import BaseConfig
+
+def random_settings_dict():
+    return {uuid.uuid4().hex:uuid.uuid4().hex for x in range(0,random.randrange(5,10))}
+
+def random_settings_many():
+    return [random_settings_dict() for x in range(0,6)]
 
 class TestConfig:
     @mock.patch("turf.config.BaseConfig.read_section_from_file")
@@ -10,3 +20,22 @@ class TestConfig:
         with mock.patch.dict("turf.config.BaseConfig.defaults", {"main":{"debug":True}}) as defaults_patch:
             BaseConfig.refresh()
             assert BaseConfig.is_debug()
+
+    @params(*random_settings_many())
+    def test_section(self, section_dict):
+        section_name = uuid.uuid4().hex
+        with mock.patch("turf.config.BaseConfig._cache", new=mock.PropertyMock(
+                return_value = {section_name:section_dict})) as cache_mock:
+            for setting_name, setting_value in section_dict.items():
+                assert BaseConfig.section(section_name)[setting_name] == setting_value
+
+    def test_section_refresh(self):
+        with mock.patch("turf.config.BaseConfig._cache", new=mock.PropertyMock(
+                return_value = {"fake_section":{}})) as cache_mock:
+            with mock.patch("turf.config.BaseConfig.refresh") as refresh_patch:
+                BaseConfig.section("fake_section")
+                refresh_patch.assert_called_one_with()
+            
+
+    
+        
