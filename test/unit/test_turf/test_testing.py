521643a9fbdbee4ffea946dbbb849f54f8019214
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-04-13 22:20:30 -0400

    add create_fake_config and test for it

---
+++ test/unit/test_turf/test_testing.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from turf.testing import create_fake_config
+
+class TestTesting(TestCase):
+    def test_fake_config(self):
+        fake_config = create_fake_config({"fake_section":{"fake_key":"fake_val"}})
+        self.assertEquals(fake_config.section("fake_section")["fake_key"], "fake_val")
