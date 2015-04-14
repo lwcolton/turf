c671a7b83529f912cddc1009c42cd0c223261fac
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-04-13 22:57:36 -0400

    testing return empty strings by default

--- src/turf/testing.py
+++ src/turf/testing.py
@@ -3,16 +3,21 @@
 from unittest import mock
 
 class FakeConfig:
-    def __init__(self, config):
+    def __init__(self, config, raise_missing=False, missing_default=""):
         self.config = config
     
     def section(self, name):
-        return self.config[name]
+        try:
+            return self.config[name]
+        except KeyError:
+            if raise_missing:
+                raise
+            else:
+               return missing_default
 
 def create_fake_config(config):
     fake_config = mock.MagicMock()
     fake_config._fake_config = FakeConfig(config)
     fake_config.section = fake_config._fake_config.section
     return fake_config
-
 
