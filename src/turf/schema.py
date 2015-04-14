297627eae114a3616edafa8cc9de306cbd8796ac
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-04-14 18:49:11 -0400

    fix shcema regex 0

--- src/turf/schema.py
+++ src/turf/schema.py
@@ -3,10 +3,10 @@
 
 
 #Bugsnag 
-bugsnag_api_key = {"type":"string", "regex":"^[a-f1-9]{32}$"}
+bugsnag_api_key = {"type":"string", "regex":"^[a-f0-9]{32}$"}
 bugsnag_release_stage = {"type":"string"}
 
 #Hipchat
-hipchat_api_key = {"type":"string", "regex":"^[a-zA-Z1-9]{40}$"}
+hipchat_api_key = {"type":"string", "regex":"^[a-zA-Z0-9]{40}$"}
 
 
