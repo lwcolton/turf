bebd6dd98e5a616b72461fcbc4b7102f56d77646
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-04-13 22:04:40 -0400

    make release easier

--- merge_release.sh
+++
@@ -1,5 +0,0 @@
-git checkout master
-git merge $BUILDBOX_COMMIT
-git push origin master
-git checkout $BUILDBOX_COMMIT
-
--- release.sh
+++ release.sh
@@ -24,3 +24,29 @@ git merge develop
 git push origin staging
 git checkout develop
 git push origin develop
+set -e
+set -u
+if [ $BUILDBOX_BRANCH = "develop" -o $BUILDBOX_BRANCH = "master" ]; then
+    diff_staging=`git diff --quiet origin/staging; echo $?`
+    if [ "$diff_staging" -eq 0 ]; then
+        echo "Exiting because this is a release"
+        exit 0
+    fi
+fi
+tox
+git checkout master
+git merge $BUILDBOX_COMMIT
+git push origin master
+git checkout $BUILDBOX_COMMIT
+
+set -e
+set -u
+branch=`git rev-parse --abbrev-ref HEAD`
+if [ "$branch" != "master" ]; then
+    echo "You may only generate pypi releases from the master branch!"
+    exit 1
+fi
+git diff --exit-code
+git pull origin master
+python setup.py register -r pypi
+python setup.py sdist upload -r pypi
