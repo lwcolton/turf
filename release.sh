5ea4a33599e621d93cb539fc5f9fca716993a191
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-04-13 22:06:14 -0400

    finish new release process

--- release.sh
+++ release.sh
@@ -6,6 +6,9 @@ if [ "$branch" != "develop" ]; then
     exit 1
 fi
 git diff --exit-code
+
+tox
+
 current_version=`cat version`
 echo "Current version: $current_version"
 read -e -p "Enter new version: " new_version
@@ -19,34 +22,11 @@ echo $new_version > version
 git add version
 git commit -m "Release $new_version"
 git tag -a $new_version -m "Release $new_version"
-git checkout staging
-git merge develop
-git push origin staging
-git checkout develop
 git push origin develop
-set -e
-set -u
-if [ $BUILDBOX_BRANCH = "develop" -o $BUILDBOX_BRANCH = "master" ]; then
-    diff_staging=`git diff --quiet origin/staging; echo $?`
-    if [ "$diff_staging" -eq 0 ]; then
-        echo "Exiting because this is a release"
-        exit 0
-    fi
-fi
-tox
-git checkout master
-git merge $BUILDBOX_COMMIT
-git push origin master
-git checkout $BUILDBOX_COMMIT
 
-set -e
-set -u
-branch=`git rev-parse --abbrev-ref HEAD`
-if [ "$branch" != "master" ]; then
-    echo "You may only generate pypi releases from the master branch!"
-    exit 1
-fi
-git diff --exit-code
-git pull origin master
+git checkout master
+git merge develop
+git push origin master
+
 python setup.py register -r pypi
 python setup.py sdist upload -r pypi
