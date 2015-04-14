1324f0bb11c31d8130e24ef9cb64abffb5c40e51
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-04-13 23:04:29 -0400

    fake_config will return default value

--- src/turf/testing.py
+++ src/turf/testing.py
@@ -5,15 +5,19 @@ from unittest import mock
 class FakeConfig:
     def __init__(self, config, raise_missing=False, missing_default=""):
         self.config = config
+        self.raise_missing = raise_missing
+        self.missing_default = missing_default
     
     def section(self, name):
         try:
             return self.config[name]
         except KeyError:
-            if raise_missing:
+            if self.raise_missing:
                 raise
             else:
-               return missing_default
+                fake_dict = mock.MagicMock()
+                fake_dict.__getitem__ = lambda x, y: self.missing_default
+                return fake_dict
 
 def create_fake_config(config):
     fake_config = mock.MagicMock()
--- test/unit/test_turf/test_testing.py
+++ test/unit/test_turf/test_testing.py
@@ -6,3 +6,7 @@ class TestTesting(TestCase):
     def test_fake_config(self):
         fake_config = create_fake_config({"fake_section":{"fake_key":"fake_val"}})
         self.assertEquals(fake_config.section("fake_section")["fake_key"], "fake_val")
+
+    def test_fake_config_missing(self):
+        fake_config = create_fake_config({})
+        self.assertEquals(fake_config.section("not_real")["nope"], "")
